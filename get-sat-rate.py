25ddd230f77b5e7ac922604bfa6846788c0645c3
Dale Visser <dale.visser@live.com>
2022-02-09 13:39:35 -0500

    Move constant up, and inline fetch call.

--- get-sat-rate.py
+++ get-sat-rate.py
@@ -1,11 +1,11 @@
 import bitcoin_value
 from datetime import datetime
 
+SAT_PER_BTC = 100_000_000
+
 print(datetime.now(), end='\n'*2)
-usd_fetcher = bitcoin_value.currency("USD")
-usd_per_btc = usd_fetcher.fetch()
+usd_per_btc = bitcoin_value.currency("USD").fetch()
 print(f"1 BTC = {usd_per_btc} USD")
-sat_per_btc = 100_000_000
-quarter_in_sat = 0.25 / usd_per_btc * sat_per_btc
+quarter_in_sat = 0.25 / usd_per_btc * SAT_PER_BTC
 print(f"25¢ ≈ {round(quarter_in_sat,1)} SAT")
 print(f"25¢/hour ≈ {round(quarter_in_sat / 60)} SAT/minute")
